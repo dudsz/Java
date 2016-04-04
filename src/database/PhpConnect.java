1268ace4a8e90b2fc17625e8aba9e4c0e191bdfd
Markus Erlach <markus.erlach@gmail.com>
2016-04-04 17:32:40 +0200

    Added delete and login, and changed response

--- src/database/PhpConnect.java
+++ src/database/PhpConnect.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
@@ -11,21 +12,24 @@ import java.util.Map;
 
 public class PhpConnect {
 	
-	String un = "detsaasta";
-    String pw = "gar";
-    String email = "gryasadsamt";
+	String username = "Sott";
+	String username2 = "Sott";
+    String pass = "salt";
+    String email = "surt";
+    String del = "del";
     
     URL url;
     HttpURLConnection conn;
     
 	public PhpConnect() throws Exception {
 	
-		urlPost();
+		urlPost(username, pass, email);
+		//urlLoginPost(username, pass);
+		//urlDeletePost(del, username2);
 		
 	}
 		
-	public void urlPost () throws Exception {
-		//URL url = new URL("http://ec2-54-191-47-17.us-west-2.compute.amazonaws.com/test_login/bla.php");
+	public void urlPost (String un, String pw, String email) throws Exception {
 		URL url = new URL("http://ec2-54-191-47-17.us-west-2.compute.amazonaws.com/test_login/insert.php");
 		
 		// Set parameters to be sent
@@ -52,10 +56,118 @@ public class PhpConnect {
 		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 		conn.getOutputStream().write(postDataBytes);
 
-		// Get response
-        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		try {
+			// Get response
+			StringBuilder sb = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			String line = null;
 
-        for (int c; (c = in.read()) >= 0;)
-            System.out.print((char)c);
+		    while ((line = reader.readLine()) != null) {
+			        sb.append(line);
+			        }
+		    System.out.println("Insert: " + sb.toString());
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} catch (Exception e) { 
+			e.printStackTrace(); 
 		}
 	}
+	
+	public void urlLoginPost (String un, String pw) throws Exception {
+		URL url = new URL("http://ec2-54-191-47-17.us-west-2.compute.amazonaws.com/test_login/login.php");
+		
+		// Set parameters to be sent
+		Map<String,Object> params = new LinkedHashMap<>();
+        params.put("un", un);
+		params.put("pw", pw);
+        
+        // Build and encode parameters
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+				
+        // Set properties of request
+		conn = (HttpURLConnection)url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		conn.getOutputStream().write(postDataBytes);
+
+		try {
+			// Get response
+			StringBuilder sb = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			String line = null;
+
+		    while ((line = reader.readLine()) != null) {
+			        sb.append(line);
+			        }
+		    System.out.println("Insert: " + sb.toString());
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} catch (Exception e) { 
+			e.printStackTrace(); 
+		}
+	}
+	
+	public void urlDeletePost (String command, String un) throws Exception {
+		URL url = new URL("http://ec2-54-191-47-17.us-west-2.compute.amazonaws.com/test_login/delete.php");
+		
+		// Set parameters to be sent
+		Map<String,Object> params = new LinkedHashMap<>();
+        params.put("delete", del);
+		params.put("un", un);
+        
+        // Build and encode parameters
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+				
+        // Set properties of request
+		conn = (HttpURLConnection)url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		conn.getOutputStream().write(postDataBytes);
+
+		try {
+			// Get response
+			StringBuilder sb = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			String line = null;
+
+		    while ((line = reader.readLine()) != null) {
+			        sb.append(line);
+			        }
+		    System.out.println("Insert: " + sb.toString());
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} catch (Exception e) { 
+			e.printStackTrace(); 
+		}
+	}
+}
+
+/*Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+//for (int c; (c = in.read()) >= 0;)
+String s = "";
+	while ((s = in.readLine()) != null) {
+        response += s;
+      }
+   // System.out.print((char)c);
+} catch (Exception e) {
+	System.out.println("Error: " + e);
+}*/
